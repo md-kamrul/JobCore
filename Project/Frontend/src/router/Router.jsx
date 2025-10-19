16d5e52ba1e279470e0fa035e544021269e09b0b
md-kamrul <join.kamrul@gmail.com>
2025-10-20 01:42:11 +0600

    update router of the files

--- Project/Frontend/src/router/Router.jsx
+++ Project/Frontend/src/router/Router.jsx
@@ -3,13 +3,21 @@ import {
 } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Profile from "../pages/Profile";
 
 export const router = createBrowserRouter([
   {
     path: "/",
         element: <Main />,
     children: [
-        { path: "/", element: <Home></Home>},
+        { path: "/", element: <Home></Home> },
+        { path: "/dashboard", element: <Dashboard></Dashboard> },
+        { path: "/login", element: <Login></Login> },
+        { path: "/signup", element: <Signup></Signup> },
+        { path: "/profile", element: <Profile></Profile>},
     ],
   },
 ]);
